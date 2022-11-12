70ed7019b0543167879e422d3df9e16322ae02bc
Sofiamondello04 <89614558+Sofiamondello04@users.noreply.github.com>
2022-11-12 15:50:53 -0300

    modifico readme

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Importar la base de datos desde PHPMyAdmin database/db_pototito.php
 
 
 Pueba con postman
-El endpoint de la API es: http://localhost/tucarpetalocal/todolist-rest/api/tasks VEEEER
+El endpoint de la API es: http://localhost/TPE-ENTREGA-2/api/products 
--- api-router.php
+++ api-router.php
@@ -7,10 +7,11 @@ $router = new Router();
 
 // defina la tabla de ruteo
 $router->addRoute('products', 'GET', 'ApiProductController', 'getProducts'); //lista todos los productos- punto 2
-$router->addRoute('products', 'GET', 'ApiProductController', 'getBrands'); // lista todas las marcas- punto 2
 $router->addRoute('products/:ID', 'GET', 'ApiProductController', 'getProduct');// muestra un solo producto
-$router->addRoute('products/:ID', 'DELETE', 'ApiProductController', 'deleteTask');
-$router->addRoute('products', 'POST', 'ApiProductController', 'insertTask'); 
+$router->addRoute('products', 'POST', 'ApiProductController', 'addProductk');// AGREGA UN PRODUCTO
+$router->addRoute('products/:ID', 'DELETE', 'ApiProductController', 'deleteProduct'); //BORRA UN PRODUCTO
+
+
 
 // ejecuta la ruta (sea cual sea)
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
