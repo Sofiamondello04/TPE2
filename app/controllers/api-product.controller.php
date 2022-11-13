a8407e17130cad927e2afffcea31dc92abb09267
Sofiamondello04 <89614558+Sofiamondello04@users.noreply.github.com>
2022-11-12 22:15:14 -0300

    agrego parametros de orden, filtros y paginacion

--- app/controllers/api-product.controller.php
+++ app/controllers/api-product.controller.php
@@ -21,9 +21,122 @@ class ApiProductController {
     }
 
     function getProducts($params = null) { 
-        $productos = $this->modelProducts->getAllProducts();
-        $this->view->response($productos);
+        
+        //Obtengo todos los productos con los parametros de ordenado, paginado y filtrado.
+
+        // Parámetros de ordenado
+        $orderBy = $_GET['orderBy'] ?? "nombre";
+        $orderMode = $_GET['orderMode'] ?? "asc";
+        
+        // Parámetros de paginado
+        $page = (int)($_GET['page'] ?? 1);
+        $limit = (int)($_GET['limit'] ?? 50); // si no me indican, por defecto muestro 50 registros
+
+        // Parámetros de filtrado
+        $filterBy = $_GET['filterBy'] ?? null;
+        $equalTo = $_GET['equalTo'] ?? null;
+
+        //Obtiene los nombres de las columnas de la tabla productos y los almacena en el arreglo $columns.
+        $columns = $this->getHeaderColumns();
+
+        // Verifica si los parámetros de ordenado son válidos
+        //in_array comprueba si el valor existe en el arreglo
+        // strtolower convierte strings a minuscula
+        if (($orderBy == 'nombre_marca' || in_array(strtolower($orderBy), $columns)) && (strtolower($orderMode == "asc") || strtolower($orderMode == "desc"))){
+
+            //Asigna un valor $order para pasar al modelo en funcion del campo por el que se quiere ordenar
+            if ($orderBy == 'nombre_marca') {
+                $order = 'marca.nombre_marca';
+            }
+            else {
+                $order = 'producto.'.$orderBy;
+            }
+            // Verifica si los parámetros de paginado son válidos
+            if((is_numeric($page) && $page>0) && (is_numeric($limit) && $limit>0)){
+
+                //Calcula cuál es el primer elemento a mostrar del paginado y lo almacena en $startAt
+                $startAt = ($page*$limit)-$limit;
+
+                // Verifica si existen los parámetros de filtrado
+                if ($filterBy!=null && $equalTo!=null){
+
+                    //Verifica que el campo $filterBy exista en la tabla (comparando con $columns)
+                    if ($filterBy == 'nombre_marca' || in_array(strtolower($filterBy), $columns)){
+
+                        //Asigna un valor $filter para pasar al modelo en funcion del campo por el que se quiere ordenar
+                        if ($filterBy == 'nombre_marca') {
+                            $filter = 'marca.nombre_marca';
+                        }
+                        else {
+                            $filter = 'producto'.$filterBy;
+                        }
+                        
+                        //Obtiene todos los productos del modelo y pasa los parametros de ordenamiento, paginado y filtrado.
+                        $response = $this->modelProducts->getAllWithFilter($order, $orderMode, $limit, $startAt, $filter, $equalTo);
+
+                        //Verifica si la consulta se realizó correctamente
+                        if($response){
+
+                            //Verifica si el resultado de la consulta está vacío.
+                            if (empty($response)) {
+                                $this->view->response("La consulta realizada no arrojó resultados", 204);
+                            }
+                            else {
+
+                                //Envía el/los producto/s a la vista para ser mostrado/s.
+                                $this->view->response($response, 200);
+                            }
+                        }
+                        else {
+
+                            //Informa error interno de servidor
+                            $response = $this->view->response("No se pudo realizar la consulta especificada.", 500);
+                        }                            
+                    }
+                    else {
+
+                        //Informa error de parámetro no válido
+                        $response = $this->view->response("Parámetro de filtrado no válido.", 400);
+                    }                        
+                }
+                else {
+
+                   //Obtiene todos los productos del modelo y pasa los parametros de ordenamiento y paginado.
+                   $response = $this->modelProducts->getAllProducts($order, $orderMode, $limit, $startAt);
+                    //var_dump($result);
+                    $this->view->response($response,200);
+                }
+                
+            }  
+            else {
+                //Informa error de parámetro no válido
+                $response = $this->view->response("Parámetro de paginado no válido.", 400);       
+            }
+        }
+        else {
+
+            //Informa error de parámetro no válido
+            $response = $this->view->response("Parámetro de ordenamiento no válido", 400);
+        }
     }
+
+    //Método que devuelve un arreglo con los nombres de las columnas de una tabla
+    function getHeaderColumns($params = null) {
+
+        //Se define un arreglo vacío para almacenar los nombres de las columnas.
+        $columns = [];
+
+        // Obtiene toda la información de las columnas de la tabla. Devuelve un arreglo de objetos con toda la info
+        $result = $this->modelProducts->getColumns();
+
+        //Recorre el arreglo y por cada elemento, extrae el nombre de la columna y lo agrega al arreglo $columns.
+        foreach ($result as $column) {
+            array_push($columns, $column->Field);
+        }
+        return $columns;
+    }
+
+
 
     function getProduct($params = null) {
         // obtengo el id del arreglo params
@@ -39,6 +152,9 @@ class ApiProductController {
         else {
             $this->view->response("El producto con el id=$id no existe", 404);
         }
+
+
+        
     }
 
     function addProduct($params = null) { //cambiarla por add comentario
--- app/models/product.model.php
+++ app/models/product.model.php
@@ -7,11 +7,35 @@ class ProductModel {
         $this->db = new PDO('mysql:host=localhost;'.'dbname=db_pototito;charset=utf8', 'root', 'root');
     }
 
-    public function getAllProducts() {
-        $query = $this->db->prepare("SELECT * FROM producto INNER JOIN marca ON producto.id_marca = marca.id_m");
+    public function getAllProducts($order, $orderMode, $limit, $startAt) {
+
+        // modifique el INNER JOIN
+        $query = $this->db->prepare("SELECT producto.*, marca.nombre_marca FROM producto 
+        INNER JOIN marca ON producto.id_marca = marca.id_m
+        ORDER BY $order $orderMode
+        LIMIT $limit
+        OFFSET $startAt");
         $query->execute();
         $productos = $query->fetchAll(PDO::FETCH_OBJ); 
         return $productos;
+    }
+
+    public function getAllWithFilter($order, $orderMode, $limit, $startAt, $filterBy, $equalTo){
+        $query = $this->db->prepare("SELECT producto.*, marca.nombre_marca FROM producto
+        INNER JOIN marca ON producto.id_marca = marca.id_m
+        WHERE $filterBy = ?
+        ORDER BY $order $orderMode
+        LIMIT $limit OFFSET $startAt");
+        $query->execute([$equalTo]);
+        $productos = $query->fetchAll(PDO::FETCH_OBJ);
+        return $productos;
+    }
+
+    public function getColumns(){
+        $query = $this->db->prepare('DESCRIBE producto');
+        $query->execute();
+        $columns = $query->fetchAll(PDO::FETCH_OBJ);
+        return $columns;
     }
 
     public function insertProduct($nombre, $precio, $talle, $id_marca) {
@@ -44,4 +68,5 @@ class ProductModel {
         $query = $this->db->prepare('UPDATE producto SET nombre= ?, talle= ?, precio= ?,  id_marca= ? WHERE id = ?');  
         $query->execute([$productoE->nombreE, $productoE->talleE, $productoE->precioE,  $productoE->id_marcaE, $productoE->id]);      
     }
+
 }
